ee8a7996ff881076fa528b6e1fe8bde7d8a5cb62
laxman7488 <35421369+laxman7488@users.noreply.github.com>
2021-07-15 18:45:37 +0530

    Delete users/server directory

--- users/server/router/login.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const routes= express.Router();
-const login=require('../controller/login')
-
-routes.route('/login').post(login.authenticate)
-routes.route('/logout').post(login.logout)
-
-module.exports=routes;
